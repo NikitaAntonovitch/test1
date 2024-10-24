ef8d8125fee8ea1253d9be300195137d9af454c2
nikit <nikitaantonovitch@gmail.com>
2024-10-24 17:23:33 +0200

    lines

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 0;
+        int b = 0;
+        if (a == 0) {
+            b = 5;
+        } else {
+            b = 0;
+        }
         //
         //
         //
